7fe7874e1871b330ca0be4e6928123bbfdaf917f
Geochi Nova <86884493+ar78studio@users.noreply.github.com>
2022-07-25 14:08:20 +0200

    Replaced <a> link Settings to <button> btn, works.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ------- Version 1 - Open Settings Window by pressing Gears Button
 const openSettingsWindow = document.getElementById("settings-gear-btn");
 const closeSettingsWindow = document.getElementById("close-window-btn");
 const settingsWindow = document.getElementById("settings-window-el");
@@ -13,11 +14,21 @@ closeSettingsWindow.addEventListener("click", function () {
   settingsWindow.classList.add("settings-window-hidden");
 });
 
-// Version 2
+// ------ Version 2 - Open Settings Window by pressing Gears Button
 // document.getElementById("settings-gear-btn").addEventListener("click", () => {
-//   document.getElementById("settings-window-el").style.display = "visible";
+//   document
+//     .getElementById("settings-window-el")
+//     .classList.remove("settings-window-hidden");
+//   document
+//     .getElementById("settings-window-el")
+//     .classList.add("settings-window-visible");
 // });
 
 // document.getElementById("close-window-btn").addEventListener("click", () => {
-//   document.getElementById("settings-window-el").style.display = "none";
+//   document
+//     .getElementById("settings-window-el")
+//     .classList.remove("settings-window-visible");
+//   document
+//     .getElementById("settings-window-el")
+//     .classList.add("settings-window-hidden");
 // });
