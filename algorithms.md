3c8557ea6c4557ef81b924f26fae517e4a6d24d8
Geochi Nova <86884493+ar78studio@users.noreply.github.com>
2022-11-24 17:42:52 +0100

    cleanup and update

--- algorithms.md
+++
@@ -1,3 +0,0 @@
-On page load, the clock is equal to null and displays 00:00
-Settings allow to set the Pomodoro, Short Break and Long Break
-Setting
--- timerLogic.js
+++ timerLogic.js
@@ -1,192 +1,198 @@
 // ------- Open Settings Window by pressing Gear Button
-const openSettingsWindow = document.getElementById('settings-gear-btn');
-const closeSettingsWindow = document.getElementById('close-window-btn');
-const settingsWindow = document.getElementById('settings-window-el');
+const openSettingsWindow = document.getElementById("settings-gear-btn");
+const closeSettingsWindow = document.getElementById("close-window-btn");
+const settingsWindow = document.getElementById("settings-window-el");
 
 // ------- Apply Settings Button
-const applySettingsBtn = document.getElementById('apply-settings');
+const applySettingsBtn = document.getElementById("apply-settings");
 
 // ------- Pomodoro Button on the Main Page
-const pomodoroBtn = document.getElementById('pomodoro-time-btn');
+const pomodoroBtn = document.getElementById("pomodoro-time-btn");
 // ------- Short Break Button on the Main Page
-const shortBreakBtn = document.getElementById('shortbreak-time-btn');
+const shortBreakBtn = document.getElementById("shortbreak-time-btn");
 // ------- Long Break Button on the Main Page
-const longBreakBtn = document.getElementById('longbreak-time-btn');
+const longBreakBtn = document.getElementById("longbreak-time-btn");
 
-openSettingsWindow.addEventListener('click', function () {
-	settingsWindow.classList.remove('settings-window-hidden');
-	settingsWindow.classList.add('settings-window-visible');
+openSettingsWindow.addEventListener("click", function () {
+  settingsWindow.classList.remove("settings-window-hidden");
+  settingsWindow.classList.add("settings-window-visible");
 });
 
-closeSettingsWindow.addEventListener('click', function () {
-	settingsWindow.classList.remove('settings-window-visible');
-	settingsWindow.classList.add('settings-window-hidden');
+closeSettingsWindow.addEventListener("click", function () {
+  settingsWindow.classList.remove("settings-window-visible");
+  settingsWindow.classList.add("settings-window-hidden");
 });
 
 // Setting up Time DOM Output
 
-const displayNumbers = document.querySelector('#clock-numbers-el');
+const displayNumbers = document.querySelector("#clock-numbers-el");
 
 // ------- Initialize user input for Pomodoro, Short Break and Long Break
 
-const pomodoroTime = document.querySelector('#pomodoro-time-el');
-const shortBreakTime = document.querySelector('#short-break-el');
-const longBreakTime = document.querySelector('#long-break-el');
+const pomodoroTime = document.querySelector("#pomodoro-time-el");
+const shortBreakTime = document.querySelector("#short-break-el");
+const longBreakTime = document.querySelector("#long-break-el");
 
 // Setting up Time Settings Object to hold values from the Pomodoro, Short Break and Long Break Time Inputs in the Settings
 const timeValueObject = {
-	pomodoroValue: pomodoroTime.value,
-	shortBreakValue: shortBreakTime.value,
-	longBreakValue: longBreakTime.value,
+  pomodoroValue: pomodoroTime.value * 1000,
+  shortBreakValue: shortBreakTime.value * 1000,
+  longBreakValue: longBreakTime.value * 1000,
 };
 
 // ------- Pass All the Values from the Settings Window on Apply Settings Button click into the timeValueObject to later be used by the Pomodoro, Short Break, Long Break buttons on the Main Page
 
-applySettingsBtn.addEventListener('click', function () {
-	timeValueObject.pomodoroValue = pomodoroTime.value;
-	timeValueObject.shortBreakValue = shortBreakTime.value;
-	timeValueObject.longBreakValue = longBreakTime.value;
-	// Pass Pomodoro Time into the Clock in the DOM upon Apply click
-	//   displayNumbers.innerHTML = `${timeValueObject.pomodoroValue}:00`;
-	console.log(timeValueObject);
-	// Close Settings Window on Apply Button click
-	settingsWindow.classList.remove('settings-window-visible');
-	settingsWindow.classList.add('settings-window-hidden');
+applySettingsBtn.addEventListener("click", function () {
+  timeValueObject.pomodoroValue = pomodoroTime.value;
+  timeValueObject.shortBreakValue = shortBreakTime.value;
+  timeValueObject.longBreakValue = longBreakTime.value;
+  // Pass Pomodoro Time into the Clock in the DOM upon Apply click
+  displayNumbers.innerHTML = `${timeValueObject.pomodoroValue}:00`;
+  console.log(timeValueObject);
+  // Close Settings Window on Apply Button click
+  settingsWindow.classList.remove("settings-window-visible");
+  settingsWindow.classList.add("settings-window-hidden");
 });
 
 // Pass Pomodoro Timer value into the Clock in the DOM
-pomodoroBtn.addEventListener('click', function () {
-	//   timeValueObject.shortBreakValue = shortBreakTime.value;
-	displayNumbers.innerHTML = `${timeValueObject.pomodoroValue}:00`;
-	clearInterval(time);
-	console.log(timeValueObject.pomodoroValue);
+pomodoroBtn.addEventListener("click", function () {
+  displayNumbers.innerHTML = `${timeValueObject.pomodoroValue / 1000}:00`;
+  clearInterval(time);
+  console.log(timeValueObject.pomodoroValue);
 });
 
 // Pass Short Break Timer value into the Clock in the DOM
-shortBreakBtn.addEventListener('click', function () {
-	//   timeValueObject.shortBreakValue = shortBreakTime.value;
-	displayNumbers.innerHTML = `${timeValueObject.shortBreakValue}:00`;
-	clearInterval(time);
-	console.log(timeValueObject.shortBreakValue);
+shortBreakBtn.addEventListener("click", function () {
+  displayNumbers.innerHTML = `${timeValueObject.shortBreakValue}:00`;
+  clearInterval(time);
+  console.log(timeValueObject.shortBreakValue);
 });
 
 // Pass Short Break Timer value into the Clock in the DOM
-longBreakBtn.addEventListener('click', function () {
-	//   timeValueObject.shortBreakValue = shortBreakTime.value;
-	displayNumbers.innerHTML = `${timeValueObject.longBreakValue}:00`;
-	clearInterval(time);
-	console.log(timeValueObject.longBreakValue);
+longBreakBtn.addEventListener("click", function () {
+  displayNumbers.innerHTML = `${timeValueObject.longBreakValue}:00`;
+  clearInterval(time);
+  console.log(timeValueObject.longBreakValue);
 });
 
 // --------------------------- Timer ------------------------
 // --------------------------- Timer ------------------------
 // --------------------------- Timer ------------------------
+
+// Time values are loaded in the settings into each button accordingly - Pomodoro, Short Break, Long Break.
+// Upon pressing each button the value is passed into the
 
 // Initializing random time Start and End to then calculate the difference using new Date() which counts the time since January 1, 1970 (ECMA epoch start)
-const start = new Date('January 1, 2022');
-const end = new Date('January 2, 2022');
+const start = new Date("January 1, 2022");
+const end = new Date("January 2, 2022");
 
 // Calculating difference
+// const difference = end.getTime() - start.getTime();
 const difference = end.getTime() - start.getTime();
+console.log(`Difference is ` + difference);
 
 const seconds = parseInt(difference / 1000) % 60;
 const minutes = parseInt(difference / 1000 / 60) % 60;
 const hours = parseInt(difference / 1000 / 60 / 60) % 24;
 
 // Declaring the button variables in the DOM
-const startBtn = document.getElementById('btn-start');
-const pauseBtn = document.getElementById('btn-pause');
+const startBtn = document.getElementById("btn-start");
+const pauseBtn = document.getElementById("btn-pause");
 
-startBtn.addEventListener('click', startTimer);
-pauseBtn.addEventListener('click', pauseTimer);
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
 
 // Declare Global Time Object to hold Starting Timer value
 const globalTimer = {
-	timer: null,
-	start: null,
-	end: null,
+  timer: null,
+  start: null,
+  end: null,
 };
 
 // START Time Calculation
 function startTimer() {
-	//   runClock();
-	// updateTimer();
-	runPomodoro();
-	startBtn.style.display = 'none';
-	pauseBtn.style.display = 'block';
-	const date = new Date();
-	globalTimer.start = date.getTime();
+  runPomodoro();
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+  const date = new Date();
+  globalTimer.start = date.getTime();
 
-	if (globalTimer.timer < 0) {
-		clearInterval(globalTimer.timer);
-	}
+  if (globalTimer.timer <= 0) {
+    clearInterval(globalTimer.timer);
+  }
 }
 
-const startTiming = timeValueObject;
-let time = startTiming.pomodoroValue * 60;
-let shortBreak = startTiming.shortBreakValue * 60;
-let longBreak = startTiming.longBreakValue * 60;
+// const startTiming = timeValueObject;
+let time = timeValueObject.pomodoroValue;
+let shortBreak = timeValueObject.shortBreakValue;
+let longBreak = timeValueObject.longBreakValue;
 console.log(time);
+
+// const timeValueObject = {
+//   pomodoroValue: pomodoroTime.value,
+//   shortBreakValue: shortBreakTime.value,
+//   longBreakValue: longBreakTime.value,
+// };
 
 // Code that starts timer
 function runPomodoro() {
-	globalTimer.timer = setInterval(updateTimer);
+  globalTimer.timer = setInterval(updateTimer);
 
-	function updateTimer() {
-		let seconds = Math.floor(time / 60);
-		let minutes = Math.floor(time / 60 / 24);
-		console.log(minutes);
+  function updateTimer() {
+    let seconds = Math.floor(time / 60);
+    let minutes = Math.floor(time / 60 / 24);
+    console.log(minutes);
 
-		// seconds = seconds < 10 ? '0' + seconds : seconds;
-		// displayNumbers.innerHTML = `${minutes}:${seconds}`;
-		seconds = seconds < 10 ? '0' + seconds : seconds;
-		displayNumbers.innerHTML = `${minutes}:${seconds}`;
-		time--;
-	}
+    // seconds = seconds < 10 ? '0' + seconds : seconds;
+    // displayNumbers.innerHTML = `${minutes}:${seconds}`;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    displayNumbers.innerHTML = `${minutes}:${seconds}`;
+    time--;
+  }
 }
 
 function runShortBreak() {
-	globalTimer.timer = setInterval(updateTimer);
+  globalTimer.timer = setInterval(updateTimer);
 
-	function updateTimer() {
-		let seconds = Math.floor(shortBreak / 60);
-		let minutes = Math.floor(shortBreak / 60 / 60);
-		console.log(minutes);
+  function updateTimer() {
+    let seconds = Math.floor(shortBreak / 60);
+    let minutes = Math.floor(shortBreak / 60 / 60);
+    console.log(minutes);
 
-		// seconds = seconds < 10 ? '0' + seconds : seconds;
-		// displayNumbers.innerHTML = `${minutes}:${seconds}`;
-		seconds = seconds < 10 ? '0' + seconds : seconds;
-		displayNumbers.innerHTML = `${minutes}:${seconds}`;
-		time--;
-	}
+    // seconds = seconds < 10 ? '0' + seconds : seconds;
+    // displayNumbers.innerHTML = `${minutes}:${seconds}`;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    displayNumbers.innerHTML = `${minutes}:${seconds}`;
+    time--;
+  }
 }
 
 function runLongBreak() {
-	globalTimer.timer = setInterval(updateTimer);
+  globalTimer.timer = setInterval(updateTimer);
 
-	function updateTimer() {
-		let seconds = Math.floor(longBreak / 60);
-		let minutes = Math.floor(longBreak / 60 / 60);
-		console.log(minutes);
-		// seconds = seconds < 10 ? '0' + seconds : seconds;
-		// displayNumbers.innerHTML = `${minutes}:${seconds}`;
-		seconds = seconds < 10 ? '0' + seconds : seconds;
-		displayNumbers.innerHTML = `${minutes}:${seconds}`;
-		time--;
-	}
+  function updateTimer() {
+    let seconds = Math.floor(longBreak / 60);
+    let minutes = Math.floor(longBreak / 60 / 60);
+    console.log(minutes);
+    // seconds = seconds < 10 ? '0' + seconds : seconds;
+    // displayNumbers.innerHTML = `${minutes}:${seconds}`;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    displayNumbers.innerHTML = `${minutes}:${seconds}`;
+    time--;
+  }
 }
 
 // PAUSE Time
 function pauseTimer() {
-	startBtn.style.display = 'block';
-	pauseBtn.style.display = 'none';
-	const date = new Date();
-	globalTimer.end = date.getTime();
-	// totalTime is the result of time passed between pressing the start and then pause button
-	(globalTimer.end - globalTimer.start) / 1000;
-	if (globalTimer.timer) {
-		clearInterval(globalTimer.timer);
-	}
+  startBtn.style.display = "block";
+  pauseBtn.style.display = "none";
+  const date = new Date();
+  globalTimer.end = date.getTime();
+  // totalTime is the result of time passed between pressing the start and then pause button
+  (globalTimer.end - globalTimer.start) / 1000;
+  if (globalTimer.timer) {
+    clearInterval(globalTimer.timer);
+  }
 }
 
 // function runClock() {
